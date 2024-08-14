f0b8b75665489b7e0dcde706558b1c81ea09c884
exfiletf <exfiletf@gmail.com>
2024-08-14 22:09:32 +0100

    Create oj.js

---
+++ oj.js
@@ -0,0 +1,28 @@
+const OJParty = require('ojparty');
+const sendEmail = require('./nm');
+
+const app = new OJParty.ojparty.app();
+
+app.post('/send-email', async (req, res) => {
+    const { subject, text, html } = req.body;
+
+    try {
+        await sendEmail({
+            host: 'mail.oepforum.com',
+            port: 587, // SMTP port
+            user: 'test@oepforum.com',
+            pass: 'FzYe5qJQWhCd',
+            to: 'exfiletf@gmail.com',
+            subject: subject || 'Default Subject',
+            text: text || 'Hello world!',
+            html: html || '<b>Hello world!</b>',
+        });
+        res.send('Email sent successfully');
+    } catch (err) {
+        res.status(500).send(`Error: ${err.message}`);
+    }
+});
+
+app.listen(8080, () => {
+    console.log('Server running on port 8080');
+});
