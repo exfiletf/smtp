4c11d3ed72aeb7f014bad58a07e02276502d825d
exfiletf <exfiletf@gmail.com>
2024-08-30 16:00:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const app = OJParty.ojparty.app();
 
 app.post('/smtp', async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin','*')
-    const {api_gmail,api_pass, to, subject, text, html } = req.body;
+    const {api_gmail,api_pass, to, subject, text, html,host,port,username,password } = req.body;
 
-    try {
+   if(host != undefined && port != undefined && username != undefined && password != undefined){
+        try {
         await sendEmail({
             host: 'smtp.gmail.com',
             port: 587, // SMTP port
@@ -26,6 +27,27 @@ app.post('/smtp', async (req, res) => {
         res.setHeader('Access-Control-Allow-Origin','*')
         res.status(500).send(`Error: ${err.message}`);
     }
+   }else if(api_gmail != undefined && api_pass != undefined){
+        try {
+        await sendEmail({
+            host: 'smtp.gmail.com',
+            port: 587, // SMTP port
+           // user: 'datamylife0014@gmail.com',
+          //  pass: 'wtgq bdtm sluh pzhn',
+           user: api_gmail,
+            pass: api_pass,
+            to: to,
+            subject: subject || 'Default Subject',
+            text: text || 'Hello world!',
+            html: html || '<b>Hello world!</b>',
+        });
+        res.setHeader('Access-Control-Allow-Origin','*')
+        res.send('Email sent successfully');
+    } catch (err) {
+        res.setHeader('Access-Control-Allow-Origin','*')
+        res.status(500).send(`Error: ${err.message}`);
+    }
+   }
 });
 
 app.listen(3090);
